5d43a8613156e071271dc81edd88e724ff74ddf1
Karim PINCHON <kpinchon@coffreo.com>
2017-07-21 15:40:34 +0200

    Use guzzle/psr7 implementation for unit tests

--- spec/Plugin/ContentTypePluginSpec.php
+++ spec/Plugin/ContentTypePluginSpec.php
@@ -23,7 +23,7 @@ class ContentTypePluginSpec extends ObjectBehavior
     function it_adds_json_content_type_header(RequestInterface $request)
     {
         $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
-        $request->getBody()->shouldBeCalled()->willReturn(json_encode(['foo' => 'bar']));
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for(json_encode(['foo' => 'bar'])));
         $request->withHeader('Content-Type', 'application/json')->shouldBeCalled()->willReturn($request);
 
         $this->handleRequest($request, function () {}, function () {});
@@ -32,7 +32,7 @@ class ContentTypePluginSpec extends ObjectBehavior
     function it_adds_xml_content_type_header(RequestInterface $request)
     {
         $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
-        $request->getBody()->shouldBeCalled()->willReturn('<foo>bar</foo>');
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('<foo>bar</foo>'));
         $request->withHeader('Content-Type', 'application/xml')->shouldBeCalled()->willReturn($request);
 
         $this->handleRequest($request, function () {}, function () {});
@@ -41,7 +41,7 @@ class ContentTypePluginSpec extends ObjectBehavior
     function it_does_not_set_content_type_header(RequestInterface $request)
     {
         $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
-        $request->getBody()->shouldBeCalled()->willReturn('foo');
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('foo'));
         $request->withHeader('Content-Type', null)->shouldNotBeCalled();
 
         $this->handleRequest($request, function () {}, function () {});
@@ -50,7 +50,7 @@ class ContentTypePluginSpec extends ObjectBehavior
     function it_does_not_set_content_type_header_if_already_one(RequestInterface $request)
     {
         $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(true);
-        $request->getBody()->shouldNotBeCalled()->willReturn('foo');
+        $request->getBody()->shouldNotBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('foo'));
         $request->withHeader('Content-Type', null)->shouldNotBeCalled();
 
         $this->handleRequest($request, function () {}, function () {});
