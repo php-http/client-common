600ea9a5d867a7bbe1c4e03722b8dc2ebc9bd3c8
Pinchon Karim <kpn13@users.noreply.github.com>
2017-08-05 17:50:10 +0200

    Add Content-type plugin to set content-type header automatically (#85)
    * Add Content-type plugin to set content-type header automatically (for now just JSON and XML)
    
    * Use guzzle/psr7 implementation for unit tests
    
    * Return early if body size is 0 or unknown
    
    * Reset the previous value at the end
    
    * Rewind stream before each content test
    
    * Add options to skip detection for too large stream
    
    * Skip detection if stream is not seekable
    
    * Add null comparison
    
    * Check if stream size is null only when skipDetection is true

---
+++ spec/Plugin/ContentTypePluginSpec.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace spec\Http\Client\Common\Plugin;
+
+use PhpSpec\Exception\Example\SkippingException;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ContentTypePluginSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Http\Client\Common\Plugin\ContentTypePlugin');
+    }
+
+    function it_is_a_plugin()
+    {
+        $this->shouldImplement('Http\Client\Common\Plugin');
+    }
+
+    function it_adds_json_content_type_header(RequestInterface $request)
+    {
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for(json_encode(['foo' => 'bar'])));
+        $request->withHeader('Content-Type', 'application/json')->shouldBeCalled()->willReturn($request);
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_adds_xml_content_type_header(RequestInterface $request)
+    {
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('<foo>bar</foo>'));
+        $request->withHeader('Content-Type', 'application/xml')->shouldBeCalled()->willReturn($request);
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_does_not_set_content_type_header(RequestInterface $request)
+    {
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('foo'));
+        $request->withHeader('Content-Type', null)->shouldNotBeCalled();
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_does_not_set_content_type_header_if_already_one(RequestInterface $request)
+    {
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(true);
+        $request->getBody()->shouldNotBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('foo'));
+        $request->withHeader('Content-Type', null)->shouldNotBeCalled();
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_does_not_set_content_type_header_if_size_0_or_unknown(RequestInterface $request)
+    {
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for());
+        $request->withHeader('Content-Type', null)->shouldNotBeCalled();
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_adds_xml_content_type_header_if_size_limit_is_not_reached_using_default_value(RequestInterface $request)
+    {
+        $this->beConstructedWith([
+            'skip_detection' => true
+        ]);
+
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('<foo>bar</foo>'));
+        $request->withHeader('Content-Type', 'application/xml')->shouldBeCalled()->willReturn($request);
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_adds_xml_content_type_header_if_size_limit_is_not_reached(RequestInterface $request)
+    {
+        $this->beConstructedWith([
+            'skip_detection' => true,
+            'size_limit' => 32000000
+        ]);
+
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('<foo>bar</foo>'));
+        $request->withHeader('Content-Type', 'application/xml')->shouldBeCalled()->willReturn($request);
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+    function it_does_not_set_content_type_header_if_size_limit_is_reached(RequestInterface $request)
+    {
+        $this->beConstructedWith([
+            'skip_detection' => true,
+            'size_limit' => 8
+        ]);
+
+        $request->hasHeader('Content-Type')->shouldBeCalled()->willReturn(false);
+        $request->getBody()->shouldBeCalled()->willReturn(\GuzzleHttp\Psr7\stream_for('<foo>bar</foo>'));
+        $request->withHeader('Content-Type', null)->shouldNotBeCalled();
+
+        $this->handleRequest($request, function () {}, function () {});
+    }
+
+}
