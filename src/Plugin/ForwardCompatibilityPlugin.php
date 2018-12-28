58487ed131d63b3f76d7425656bb6047ba1b4b02
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-12-28 11:18:30 +0100

    Added a way to be forward compatible with version 2.0

---
+++ src/Plugin/ForwardCompatibilityPlugin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Http\Client\Common\Plugin;
+
+use Http\Client\Common\Plugin;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * A plugin that helps you migrate from php-http/client-common 1.x to 2.x. This
+ * will also help you to support PHP5 at the same time you support 2.x.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+abstract class ForwardCompatibilityPlugin implements Plugin
+{
+    abstract function doHandleRequest(RequestInterface $request, callable $next, callable $first);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleRequest(RequestInterface $request, callable $next, callable $first)
+    {
+        return $this->doHandleRequest($request, $next, $first);
+    }
+}
