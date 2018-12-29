94724848a678bb43786e5fd5708f316c8bfba012
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-12-29 11:54:47 +0100

    Added backwards compatibility layer (#133)

--- src/Plugin/VersionBridgePlugin.php
+++ src/Plugin/VersionBridgePlugin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Http\Client\Common\Plugin;
 
+use Http\Promise\Promise;
 use Psr\Http\Message\RequestInterface;
 
 /**
@@ -16,7 +17,7 @@ trait VersionBridgePlugin
 {
     abstract protected function doHandleRequest(RequestInterface $request, callable $next, callable $first);
 
-    public function handleRequest(RequestInterface $request, callable $next, callable $first)
+    public function handleRequest(RequestInterface $request, callable $next, callable $first): Promise
     {
         return $this->doHandleRequest($request, $next, $first);
     }
