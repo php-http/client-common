7d5fe4deb07494ddddc49296972de8adfcc59bd6
Nyholm <tobias.nyholm@gmail.com>
2018-12-31 12:15:51 +0100

    init

---
+++ tests/PluginClientTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Http\Client\Common;
+
+use PHPUnit\Framework\TestCase;
+
+class PluginClientTest extends TestCase
+{
+    private $syncClient;
+    private $asyncClient;
+
+    protected function setUp()
+    {
+
+    }
+}
